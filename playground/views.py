ae6d825bc74a2c964451bf6938b4d2c5e9cdcb2c
Rio Edwards <rioredwards@gmail.com>
2023-10-06 18:12:17 -0700

    Add name variable to template

--- playground/views.py
+++ playground/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 # Create your views here.
 def say_hello(request):
     # return HttpResponse("Hello World!")
-    return render(request, "hello.html")
+    return render(request, "hello.html", {"name": "Rio"})
